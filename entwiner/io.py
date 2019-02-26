fe4aaf5aebfac2718e6a2c2707508574de889c77
= <=>
2019-02-26 11:48:07 -0800

    Source layer is now embedded in edges

--- entwiner/io.py
+++ entwiner/io.py
@@ -1,6 +1,7 @@
 """Wraps various readers/writers for different geospatial formats with a focus on
 low-memory reading."""
 import copy
+import os
 
 import fiona
 
@@ -10,10 +11,12 @@ class UnknownGeometryError(ValueError):
 
 
 def edge_generator(path, precision, rev=False):
+    layer = os.path.splitext(os.path.basename(path))[0]
     with fiona.open(path) as handle:
         for f in handle:
             props = dict(f["properties"])
             props["_geometry"] = to_wkt(f["geometry"])
+            props["_layer"] = layer
 
             u = ", ".join(
                 [str(round(c, precision)) for c in f["geometry"]["coordinates"][0]]
