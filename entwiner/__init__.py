e41f3198cfd52c3e774e5a9e5ffd41de3e4df64f
= <=>
2020-11-11 15:31:35 -0800

    Moved geopackage to separate module

--- entwiner/__init__.py
+++ entwiner/__init__.py
@@ -2,6 +2,7 @@ from . import constants
 from . import io
 from .graphs.digraphdb import DiGraphDB, DiGraphDBView
 from .build import GraphBuilder
+from .geopackage import GeoPackage
 from .geopackagenetwork import GeoPackageNetwork
 
 __version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     author_email='nbolten@gmail.com',
     license='MIT',
     entry_points={"console_scripts": ["entwiner = entwiner.cli:entwiner"]},
-    packages=['entwiner', 'entwiner.graphs'],
+    packages=['entwiner', 'entwiner.geopackage', 'entwiner.graphs'],
     package_dir={"": "."},
     package_data={},
     install_requires=['click==7.*,>=7.0.0', 'fiona==1.*,>=1.8.13', 'geomet==0.*,>=0.2.1', 'networkx==2.*,>=2.4.0', 'pyproj==2.*,>=2.4.2', 'shapely==1.*,>=1.6.4'],
